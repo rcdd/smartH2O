a9a95d3534dddb7979d299884718c6bc468bc6af
RubenPereira <rpbeat@gmail.com>
2016-11-21 17:55:20 +0000

    SmartH2O_DU

---
+++ SmartH2O_DU/Service1.svc.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using SensorNodeDll;
+using System.Net;
+//using uPLibrary.Networking.M2Mqtt;
+
+namespace SmartH2O_DU
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public string GetAllValues()
+        {
+            SensorNodeDll.SensorNodeDll dll = new SensorNodeDll.SensorNodeDll();
+            dll.Initialize(receiverSensor, 1000);
+            return null;
+        }
+
+        public void receiverSensor(string str)
+        {
+            string[] s = str.Split(';');
+            foreach (string word in s)
+            {
+                Console.WriteLine(word);
+            }
+            Console.Write(str);
+        }
+
+
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
