45a7c0baa73e5fdf4dbbfc88a84e48a8f8c28da7
ruben <2140816@MY.IPLEIRIA.PT>
2016-12-13 00:30:34 +0000

    Merge all project

---
+++ SmartH2O_DU/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using uPLibrary.Networking.M2Mqtt;
+
+namespace SmartH2O_DU
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SensorNodeDll.SensorNodeDll dll = new SensorNodeDll.SensorNodeDll();
+            dll.Initialize(DllValues, 5000);
+        }
+
+        private static void DllValues(string message)
+        {
+            if (message != null)
+            {
+                string[] words = message.Split(';');
+                string ID="";
+                string Value="";
+                string Desc="";
+                int i=0;
+                foreach (string word in words)
+                {
+                    if (i == 0)
+                    {
+                        ID = word;
+                    }else if(i == 1)
+                    {
+                        Desc = word;
+                    }else
+                    {
+                        Value = word;
+                    }
+                    i++;
+                }
+                XElement element = new XElement("Sensor", new XElement("ID", ID), new XElement("Name", Desc), new XElement("Value", Value), new XElement("Time", DateTime.Now.ToString("HH:mm:ss")), new XElement("Date", DateTime.Now.ToString("dd/MM/yyyy")));
+                Console.WriteLine(element);
+                publish(element.ToString());
+            }
+        }
+
+        private static void publish(string v)
+        {
+            MqttClient client = new MqttClient("127.0.0.1");
+            client.Connect(Guid.NewGuid().ToString());
+            if (client.IsConnected)
+            {
+                client.Publish("SensorValues", Encoding.UTF8.GetBytes(v));
+            }
+        }
+    }
+}
---
+++ SmartH2O_Service/Service1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Xml;
+
+namespace SmartH2O_Service
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class Service1 : IService1
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+        public string GetAllValues()
+        {
+            string XmlPath = Path.GetDirectoryName(Path.GetDirectoryName(System.IO.Directory.GetCurrentDirectory())) + "\\App_Data\\log-sensors.xml";
+            XmlDocument doc = new XmlDocument();
+            if (File.Exists(XmlPath))
+            { 
+                doc.Load(XmlPath);
+                XmlNodeList elemList = doc.GetElementsByTagName("sensor");
+                for (int i = 0; i < elemList.Count; i++)
+                {
+                    Console.WriteLine(elemList[i].InnerXml);
+                }
+                return "I found the file!"+doc.Name;
+            }else
+            {
+                return "File does not exists yet";
+            }
+        }
+    }
+}
