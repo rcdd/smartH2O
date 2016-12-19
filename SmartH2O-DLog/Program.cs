10822cb0c4ca8309b7e87600e638c577e4bf8b48
RubenPereira <rpbeat@gmail.com>
2016-12-19 20:07:51 +0000

    again bebe :P

--- SmartH2O-DLog/Program.cs
+++ SmartH2O-DLog/Program.cs
@@ -18,17 +18,26 @@ namespace SmartH2O_DLog
         {
             MqttClient client = new MqttClient("host.dynip.sapo.pt", 21, false, null, null, MqttSslProtocols.None);
             client.Connect(Guid.NewGuid().ToString(), "isuser", "is2016");
+            MqttClient client2 = new MqttClient("host.dynip.sapo.pt", 21, false, null, null, MqttSslProtocols.None);
+            client2.Connect(Guid.NewGuid().ToString(), "isuser", "is2016");
             if (client.IsConnected)
             {
                 //Subscribe
                 string[] m_strTopicsInfoLog = new string[1];
                 string[] m_strTopicsInfoAlarm = new string[1];
                 m_strTopicsInfoLog[0] = "SensorValues";
-                m_strTopicsInfoAlarm[0] = ""
+                m_strTopicsInfoAlarm[0] = "Alarms";
 
-                client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+               
                 byte[] qosLevels = { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE };
+
+
                 client.Subscribe(m_strTopicsInfoLog, qosLevels);
+                client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+
+
+                client2.Subscribe(m_strTopicsInfoAlarm, qosLevels);
+                client2.MqttMsgPublishReceived += client_MqttMsgPublishReceivedAlarms;
             }
             else
             {
@@ -44,7 +53,7 @@ namespace SmartH2O_DLog
 
             ServiceLog.ServiceLogClient service = new ServiceLog.ServiceLogClient();
             Console.Write(service.SendValues(strTemp));
-            Console.Write(service.GetAllValues());
+            //Console.Write(service.GetAllValues());
 
             /* Console.Write("Received msg: ");
              Console.WriteLine(t.Element("Name").Value);
@@ -52,6 +61,24 @@ namespace SmartH2O_DLog
              Console.WriteLine(t.Element("ID").Value);
              Console.WriteLine(t.Element("Date").Value);
              Console.WriteLine(t.Element("Time").Value+"\n\n");*/
+        }
+
+        private static void client_MqttMsgPublishReceivedAlarms(object sender, MqttMsgPublishEventArgs e)
+        {
+            String strTemp = Encoding.UTF8.GetString(e.Message);
+            XElement t = XElement.Parse(strTemp);
+
+            //ServiceLog.ServiceLogClient service = new ServiceLog.ServiceLogClient();
+            //Console.Write(service.SendValues(strTemp));
+            //Console.Write(service.GetAllValues());
+
+             Console.Write("Received msg: ");
+             Console.WriteLine(t.Element("Name").Value);
+             Console.WriteLine(t.Element("Value").Value);
+             Console.WriteLine(t.Element("ID").Value);
+             Console.WriteLine(t.Element("Date").Value);
+             Console.WriteLine(t.Element("Alarm").Value);
+             Console.WriteLine(t.Element("Time").Value+"\n\n");
         }
     }
 }
